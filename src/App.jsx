5bb654aae13511342ee07794953fca711aa94593
Arun <arundevweb7@outlook.com>
2025-08-06 13:35:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import WeatherCard from './components/weather'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+  <div className="dashboard">
+      <div className="weather-section">
+        <WeatherCard />
+      </div>
+  
+    </div>
+  )
+}
+
+export default App
